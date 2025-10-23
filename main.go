a947c9a9a1e05c9dc815bb9db3c2b4d98d9d7bbf
Baymunammet <baymuhammet@example.com>
2025-10-23 18:50:17 +0500

    feat: write hello world

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Akmyrat03/api/crud/internal/api"
 	"github.com/gofiber/fiber/v2"
 )
@@ -12,4 +14,5 @@ func main() {
 	api.Routes(app)
 
 	app.Listen(":3000")
+	fmt.Println("Hello world")
 }
